990e0c59e08a2bdab58415c26c89ca34021ba850
tsunetoki <hi-g.g.908@softbank.ne.jp>
2016-01-04 16:24:52 +0900

    Create 1129.cpp

---
+++ 1129.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define REEP(i, a, b) for(int i=(a); i<(b); i++)
+#define REP(i, n) REEP(i, 0, n)
+#define ALL(a) (a).begin(),(a).end()
+using namespace std;
+
+int solve(int n, int r){
+  int p, c, array[n], tmp[n];
+  REP(i, n) array[i]=n-i;
+  REP(i, r){
+    cin >> p >> c;
+    REP(j, n) tmp[j]=array[j];
+    REP(j, c) array[j]=tmp[p+j-1];
+    REP(j, p-1) array[c+j]=tmp[j];
+  }
+  return array[0];
+}
+
+int main(void){
+  int n, r;
+  while(cin >> n >> r, !( n==0 && r==0 )){
+    cout << solve(n, r) << endl;
+  }
+  return 0;
+}
